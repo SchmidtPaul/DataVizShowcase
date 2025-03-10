0bb97ff8a03e756df6b16e0261307e076f736b9b
Paul Schmidt <schmidtpaul@hotmail.de>
2025-03-10 21:29:16 +0100

    beekeeper v1.0

--- beekeeper/code_beekeeper.R
+++ beekeeper/code_beekeeper.R
@@ -24,12 +24,16 @@ library(scales)
 library(showtext)
 library(tidyverse)
 
+conflicts_prefer(dplyr::filter)
+
+set.seed(43)
+
 # Font setup
+showtext_opts(dpi = 300)
 showtext_auto()
 font_add_google(name = "Fira Sans", family = "Fira")
 
-set.seed(43)
-colors <- c(grey = "#492726", yellow = "#fabb00", beige = "#f0f1f1", blue = "#124E78", red = "#6e0e0a", orange = "#F19143")
+colors <- c(brown = "#492726", yellow = "#fabb00", grey = "#f0f1f1")
 
 # Data import & prep ------------------------------------------------------
 dat <- read_csv(
@@ -47,42 +51,42 @@ dat <- read_csv(
 ## Plot on the left -------------------------------------------------------
 dat_left <- dat %>%
   select(id, age_now, age_start) %>%
-  pivot_longer(-id) %>%
-  mutate(
-    age_now = as.factor(name) %>%
-      fct_recode(!!!c("When they started" = "age_start", "Currently" = "age_now")) %>%
-      fct_relevel(c("When they started", "Currently")),
-    name = NULL
-  ) %>% 
+  pivot_longer(
+    cols = c(age_start, age_now),
+    names_to = "time_point",
+    values_to = "age"
+  ) %>%
+  mutate(time_point = factor(
+    time_point,
+    levels = c("age_start", "age_now"),
+    labels = c("When they started", "Currently")
+  )) %>%
   na.omit()
 
-summary_left <- dat_left %>% 
-  summarize(mean = mean(value), .by = age_now) %>% 
+summary_left <- dat_left %>%
+  summarize(mean = mean(age), .by = time_point) %>%
   mutate(mean_label = glue("{round(mean)} years"))
 
-lab_left <- dat_left %>% 
-  mutate(min70 = value>=70) %>% 
-  group_by(age_now) %>% 
-  count(min70) %>% 
-  mutate(p = n/sum(n),
-         perc = percent(p, accuracy = 1, suffix = " %")) %>% 
-  ungroup()
+lab_left <- dat_left %>%
+  filter(time_point == "Currently") %>%
+  summarize(perc = percent(mean(age >= 70), accuracy = 1)) %>% 
+  pull(perc)
 
 arrow_left <- tibble(
   x = 1.5,
-  y = 83,
+  y = 85,
   xend = 1.75,
   yend = 75,
-  label = glue("{lab_left[4, 'perc']} of all beekeepers<br>are at least 70 years old.")
+  label = glue("{lab_left} of all beekeepers<br>are at least 70 years old.")
 )
 
 
-p_left <- ggplot(dat_left,
-                aes(y = value, x = age_now)) +
+p_left <- ggplot(dat_left) +
+  aes(y = age, x = time_point) +
   # Hexagons
   geom_star(
     aes(fill = "all"),
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     position = position_jitter(width = 0.2),
     starshape = "hexagon",
     alpha = 0.9,
@@ -107,9 +111,9 @@ p_left <- ggplot(dat_left,
   # Mean Bar
   geom_errorbar(
     data = summary_left,
-    aes(x = age_now, ymax = mean, ymin = mean),
+    aes(x = time_point, ymax = mean, ymin = mean),
     linewidth = 0.5,
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     linetype = "longdash",
     inherit.aes = FALSE,
     width = 0.95
@@ -117,12 +121,12 @@ p_left <- ggplot(dat_left,
   # Mean label pt.1
   geom_richtext(
     data = summary_left,
-    aes(x = age_now, y = mean, label = mean_label),
+    aes(x = time_point, y = mean, label = mean_label),
     hjust = 0, vjust = -0.1,
     nudge_x = -0.48,
     family = "Fira",
     size = 10 * 0.35,
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     fill = "white",
     label.color = NA,
     label.padding = unit(0.02, "lines")
@@ -130,12 +134,12 @@ p_left <- ggplot(dat_left,
   # Mean label pt.2
   geom_richtext(
     data = summary_left,
-    aes(x = age_now, y = mean, label = "on average"),
+    aes(x = time_point, y = mean, label = "on average"),
     hjust = 0, vjust = 1.2,
     nudge_x = -0.48,
     family = "Fira",
     size = 10 * 0.35,
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     fill = "white",
     label.color = NA,
     label.padding = unit(0.02, "lines")
@@ -152,8 +156,8 @@ p_left <- ggplot(dat_left,
     vjust = 1, nudge_y = 0,
     family = "Fira",
     size = 9 * 0.35,
-    color = colors[["grey"]],
-    fill = colors[["beige"]],
+    color = colors[["brown"]],
+    fill = colors[["grey"]],
     label.color = NA,
     label.padding = unit(0.25, "lines")
   ) +
@@ -168,7 +172,7 @@ p_left <- ggplot(dat_left,
     ),
     linewidth = 0.5,
     curvature = 0.5,
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     arrow = arrow(30L, unit(0.075, "inches"),
                   "last", "closed"),
     inherit.aes = FALSE
@@ -181,36 +185,39 @@ p_left <- ggplot(dat_left,
     hjust = 0.05,
     vjust = 0.575,
     size = 12*0.35,
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     fill = "white",
     label.color = NA
   ) +
+  # Format y-axis
   scale_y_continuous(
     limits = c(0, NA),
     breaks = seq(0, 90, 10),
     expand = expansion(mult = c(0, 0.02))
   ) +
+  # Format x-axis
   scale_x_discrete(
     name = NULL,
     labels =
-      c("age_start" = "How old were you when you started beekeeping?", "age_now" = "How old are you?")
-  )+
+      c("age_start" = "How old were you when you started beekeeping?", 
+        "age_now" = "How old are you?")
+  ) +
   coord_cartesian(clip = 'off') +
   theme_classic() +
   theme(
     axis.line = element_blank(),
     axis.title.y = element_blank(),
     axis.ticks = element_blank(),
-    axis.text.y = element_text(family = "Fira", color = colors[["grey"]], size = 12, face = "bold"),
-    axis.text.x = element_text(family = "Fira", color = colors[["grey"]], size = 12),
-    panel.grid.major.y = element_line(linetype = "dotted", color = colors[["grey"]], linewidth = 0.3),
+    axis.text.y = element_text(family = "Fira", color = colors[["brown"]], size = 12, face = "bold"),
+    axis.text.x = element_text(family = "Fira", color = colors[["brown"]], size = 12),
+    panel.grid.major.y = element_line(linetype = "dotted", color = colors[["brown"]], linewidth = 0.3),
     legend.title = element_blank(),
-    legend.text = element_text(face = "bold", family = "Fira", color = colors[["grey"]], size = 12),
+    legend.text = element_text(face = "bold", family = "Fira", color = colors[["brown"]], size = 12),
     legend.position = c(1, 0.011),
     legend.justification = c(1, 0),
     legend.margin = margin(t = 0, r = 5, b = 5, l = 0),
     legend.background = element_rect(
-      fill = colors[["beige"]],
+      fill = colors[["grey"]],
       colour = NA,
       size = 0.1
     )
@@ -239,18 +246,20 @@ summary_right <- dat_right %>%
          perc = percent(p, accuracy = 0.1, suffix = " %"))
 
 arrow_right <- tibble(
-  x = 125,
+  x = 110,
   y = 13,
   xend = 52.5,
   yend = 11.75,
-  label = "During the period of German reunification, significantly fewer people temporarily started beekeeping."
-) %>% mutate(label = str_wrap(label, 50) %>% str_replace_all("\n", "<br>"))
+  label = "During the period of German reunification,<br>significantly fewer people temporarily started beekeeping."
+)
 
 p_right <-
-  ggplot(summary_right, aes(x = n, y = start_fac, label = perc)) +
+  ggplot(data = summary_right) +
+  aes(x = n, y = start_fac, label = perc) +
+  # Bars
   geom_bar(
     stat = "identity",
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     fill = colors[["yellow"]],
     show.legend = FALSE
   ) +
@@ -261,7 +270,7 @@ p_right <-
     nudge_x = 1,
     family = "Fira",
     size = 10 * 0.35,
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     fill = "white",
     label.color = NA,
     label.padding = unit(0.1, "lines")
@@ -278,8 +287,8 @@ p_right <-
     vjust = 1, nudge_y = 0.25,
     family = "Fira",
     size = 9 * 0.35,
-    color = colors[["grey"]],
-    fill = colors[["beige"]],
+    color = colors[["brown"]],
+    fill = colors[["grey"]],
     label.color = NA,
     label.padding = unit(0.25, "lines")
   ) +
@@ -294,13 +303,17 @@ p_right <-
     ),
     size = 0.5,
     curvature = 0.15,
-    color = colors[["grey"]],
+    color = colors[["brown"]],
     arrow = arrow(30L, unit(0.075, "inches"),
                   "last", "closed"),
     inherit.aes = FALSE
   ) +
-  scale_y_discrete(name = "When did you start beekeeping?*",
-                   expand = expansion(mult = c(0.01, 0.01))) +
+  # Format y-axis
+  scale_y_discrete(
+    name = "When did you start beekeeping?*",
+    expand = expansion(mult = c(0.01, 0.01))
+  ) +
+  # Format x-axis
   scale_x_continuous(
     name = "Number of beekeepers",
     expand = expansion(mult = c(0, 0.1)),
@@ -314,72 +327,87 @@ p_right <-
     axis.text.x = element_text(
       face = "bold",
       family = "Fira",
-      color = colors[["grey"]],
+      color = colors[["brown"]],
       size = 12
     ),
     axis.text.y = element_text(
       face = "bold",
       family = "Fira",
-      color = colors[["grey"]],
+      color = colors[["brown"]],
       size = 12
     ),
     axis.title.x = element_text(
       family = "Fira",
-      color = colors[["grey"]],
+      color = colors[["brown"]],
       size = 14
     ),
     axis.title.y = element_text(
       family = "Fira",
-      color = colors[["grey"]],
+      color = colors[["brown"]],
       size = 14
     ),
     panel.grid.major.x = element_line(
       linetype = "dotted",
-      color = colors[["grey"]],
+      color = colors[["brown"]],
       linewidth = 0.3)
   )
 
+
+# Import and add bee image ------------------------------------------------
 bee_img <- image_read("beekeeper/honeybee.png") %>% 
   image_rotate(-90) %>% 
   rasterGrob()
 
 p_right <- p_right +
-  annotation_custom(bee_img, xmin = 180, xmax = 260, ymin = -Inf, ymax = 10)
+  annotation_custom(
+    grob = bee_img,
+    xmin = 180,
+    xmax = 260,
+    ymin = -Inf,
+    ymax = 10
+  )
+
 
 # Gesamt ------------------------------------------------------------------
 title <- "Beekeeping in Mecklenburg-Vorpommern, Germany"
-subtitle <- "The Epidemiological Service/Animal Disease Control Service of the State Office for Agriculture, Food Safety and Fisheries (LALLF) MV conducted a survey on the current situation of beekeeping in Mecklenburg-Vorpommern at the end of 2015. Over 1,200 beekeepers in the state provided information on various aspects of beekeeping using a questionnaire."
+subtitle <- "The 'Landesamt für Landwirtschaft, Lebensmittelsicherheit
+und Fischerei' conducted a survey on the situation of beekeeping in Mecklenburg-Vorpommern at the end of 2015. Over 1,200 beekeepers in the state provided information on various aspects of beekeeping using a questionnaire."
 caption <- "*The year category labels are simplified: The category '\U2264 2005' for example only refers to the years 2001-2005.\nSource: Beekeeper Survey MV 2015 Results (BioMath, 2016): https://www.lallf.de/fileadmin/media/PDF/tiergseuchendia/epidem/Final__Imkerumfrage_MV_2015_Ergebnisse.pdf; Figure: https://www.dlf.pt/ddetail/hxRRJRJ_transparent-honey-bee-png-bee-drawing-png-png/"
 
 p <- p_left + p_right +
-  plot_annotation(title = title,
-                  subtitle = str_wrap(subtitle, 150),
-                  caption = caption,
-                  theme = theme(
-                    plot.title = element_text(
-                      size = 24,
-                      family = "Calibri",
-                      face = "bold",
-                      color = colors[["grey"]]
-                    ),
-                    plot.subtitle = element_text(
-                      size = 12,
-                      family = "Calibri",
-                      color = colors[["grey"]]
-                    ),
-                    plot.caption = element_text(
-                      size = 7,
-                      family = "Calibri",
-                      color = colors[["grey"]]
-                    )
-                  ))
+  plot_annotation(
+    title = title,
+    subtitle = str_wrap(subtitle, 150),
+    caption = caption,
+    theme = theme(
+      plot.title = element_text(
+        size = 24,
+        family = "Fira",
+        face = "bold",
+        color = colors[["brown"]]
+      ),
+      plot.subtitle = element_text(
+        size = 12,
+        family = "Fira",
+        color = colors[["brown"]]
+      ),
+      plot.caption = element_text(
+        size = 7,
+        family = "Fira",
+        color = colors[["brown"]]
+      )
+    )
+  )
 
-BioMathR::gg_export(
-  plot_obj = p,
-  file_name = "Imker",
-  width_cm = 32,
-  height_cm = 18,
-  pdf = "open",
-  png = "none",
-  svg = "none"
-)
+
+# Export ------------------------------------------------------------------
+ggsave(
+  filename = "beekeeper/beekeeper.png", 
+  plot = p,
+  width = 32, 
+  height = 18, 
+  units = "cm", 
+  dpi = 300
+  )
+
+# system('open "beekeeper/beekeeper.png"')
